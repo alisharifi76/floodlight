53aaed9112976a2bb137d3178c86906799e542a3
alisharifi76 <alisharif7615@gmail.com>
2019-07-13 02:13:07 +0430

    OSPf added!

--- src/main/java/net/floodlightcontroller/mactracker/MACTracker.java
+++ src/main/java/net/floodlightcontroller/mactracker/MACTracker.java
@@ -2,21 +2,26 @@ package net.floodlightcontroller.mactracker;
 
 import java.util.*;
 
+import net.floodlightcontroller.core.internal.IOFSwitchService;
 import net.floodlightcontroller.core.types.NodePortTuple;
 import net.floodlightcontroller.devicemanager.IDevice;
 import net.floodlightcontroller.devicemanager.IDeviceService;
 import net.floodlightcontroller.devicemanager.SwitchPort;
 import net.floodlightcontroller.linkdiscovery.ILinkDiscoveryService;
 import net.floodlightcontroller.linkdiscovery.Link;
+import net.floodlightcontroller.staticentry.IStaticEntryPusherService;
 import net.floodlightcontroller.statistics.IStatisticsService;
 import net.floodlightcontroller.topology.ITopologyService;
 import net.floodlightcontroller.util.OFMessageUtils;
+import org.projectfloodlight.openflow.protocol.OFFlowAdd;
 import org.projectfloodlight.openflow.protocol.OFMessage;
 import org.projectfloodlight.openflow.protocol.OFPacketIn;
 import org.projectfloodlight.openflow.protocol.OFType;
-import org.projectfloodlight.openflow.types.IPv4Address;
-import org.projectfloodlight.openflow.types.IPv6Address;
-import org.projectfloodlight.openflow.types.MacAddress;
+import org.projectfloodlight.openflow.protocol.action.OFAction;
+import org.projectfloodlight.openflow.protocol.action.OFActionOutput;
+import org.projectfloodlight.openflow.protocol.match.Match;
+import org.projectfloodlight.openflow.protocol.match.MatchField;
+import org.projectfloodlight.openflow.types.*;
 import net.floodlightcontroller.flowcache.*;
 
 import net.floodlightcontroller.core.FloodlightContext;
@@ -37,10 +42,12 @@ import net.floodlightcontroller.accesscontrollist.ACLRule;
 import net.floodlightcontroller.core.IFloodlightProviderService;
 
 import java.util.concurrent.ConcurrentSkipListSet;
+import java.util.concurrent.TimeUnit;
 
 import net.floodlightcontroller.packet.Ethernet;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import sun.font.TrueTypeFont;
 
 import static jdk.internal.net.http.common.Utils.stringOf;
 
@@ -57,6 +64,9 @@ public class MACTracker implements IOFMessageListener, IFloodlightModule {
     protected ILinkDiscoveryService iLinkDiscoveryService;
     protected IRoutingService iRoutingService;
     protected ACLRule rule;
+    private ArrayList<OSPFNode> OSPFNodes;
+    protected IOFSwitchService switchService;
+    protected IStaticEntryPusherService sfp;
 
     @Override
     public String getName() {
@@ -95,6 +105,9 @@ public class MACTracker implements IOFMessageListener, IFloodlightModule {
         l.add(IStatisticsService.class);
         l.add(ITopologyService.class);
         l.add(ILinkDiscoveryService.class);
+        l.add(IOFSwitchService.class);
+        l.add(IStaticEntryPusherService.class);
+
         return l;
     }
 
@@ -107,15 +120,19 @@ public class MACTracker implements IOFMessageListener, IFloodlightModule {
         topologyService = context.getServiceImpl(ITopologyService.class);
         deviceManagerService = context.getServiceImpl(IDeviceService.class);
         iLinkDiscoveryService = context.getServiceImpl(ILinkDiscoveryService.class);
-        iRoutingService = context.getServiceImpl(IRoutingService.class);
-        rule = new ACLRule();
+        OSPFNodes = new ArrayList<>();
+        sfp = context.getServiceImpl(IStaticEntryPusherService.class);
+        switchService = context.getServiceImpl(IOFSwitchService.class);
+
     }
 
     @Override
     public void startUp(FloodlightModuleContext context) {
         iStatisticsService.collectStatistics(true);
         floodlightProvider.addOFMessageListener(OFType.PACKET_IN, this);
-        floodlightProvider.addOFMessageListener(OFType.HELLO, this);
+        discoverTopology();
+        OSPF();
+
 
     }
 
@@ -125,23 +142,24 @@ public class MACTracker implements IOFMessageListener, IFloodlightModule {
 
         if(cnt == 0) {
 //            logger.info(String.valueOf(iLinkDiscoveryService.getLinks().keySet().toArray().length));
-            for(Link ll : iLinkDiscoveryService.getLinks().keySet()) {
+            for(DatapathId ll : topologyService.getAllLinks().keySet()) {
 //                logger.info(ll.toString());
             }
-            cnt++;
+//            cnt++;
         }
-//        logger.info(String.valueOf(iRoutingService.getMaxPathsToCompute()));
 //        logger.info(sw.getId().toString());
-//        logger.info(sw.getAttributes().toString());
-        for (IDevice d : allDevices) {
-            SwitchPort[] ii= d.getAttachmentPoints();
-            IPv4Address[] jj = d.getIPv4Addresses();
-            for (int j = 0; j < d.getIPv4Addresses().length; j++) {
-//                logger.info(ii[j].toString());
+//        if(cnt==0) {
+            for (IDevice d : allDevices) {
+                SwitchPort[] ii = d.getAttachmentPoints();
+                IPv4Address[] jj = d.getIPv4Addresses();
+                for (int j = 0; j < ii.length; j++) {
+//                    logger.info(ii[j].getNodeId().toString());
 //                logger.info(jj[j].toString());
-            }
-        }
+                }
 
+            }
+//            cnt++;
+//        }
 
 //        logger.info(topologyService.getAllLinks().toString());
         Set<NodePortTuple> k = iStatisticsService.getBandwidthConsumption().keySet();
@@ -167,5 +185,111 @@ public class MACTracker implements IOFMessageListener, IFloodlightModule {
         }
         return Command.CONTINUE;
     }
+    private void discoverTopology(){
+//        HashMap<String, Integer> deviceCheck = new HashMap<>();
+//        boolean has = false;
+//
+//        while (true)
+//            if (!topologyService.getAllLinks().isEmpty() && !(deviceManagerService.getAllDevices().size()<1))
+//                break;
+//        int count = 0;
+//
+//        Collection<? extends IDevice> allDevices = deviceManagerService.getAllDevices();
+//        for (IDevice d : allDevices) {
+//            SwitchPort[] ii = d.getAttachmentPoints();
+//            if (d.getMACAddressString().substring(0,2).equals("00")){
+//                logger.info("logggggggggggggggg " + ii[0].getNodeId());
+//                for (int i = 0; i < OSPFNodes.size(); i++){
+//                    if (OSPFNodes.get(i).getSWID().equals(ii[0].getNodeId())){
+//                        OSPFNodes.get(i).addToAccesible(d.getIPv4Addresses()[0], 0, ii[0].getPortId());
+//                        has = true;
+//                    }
+//                }
+//                if (!has){
+//                    OSPFNode newNode = new OSPFNode(ii[0].getNodeId());
+//                    newNode.addToAccesible(d.getIPv4Addresses()[0], 0, ii[0].getPortId());
+//                    OSPFNodes.add(newNode);
+//                    has = false;
+//                }
+//            }
+//        }
+//        logger.info("sizeeeeeeeeeeeeeeeeee " + OSPFNodes.size());
+//        for (int i = 0; i < OSPFNodes.get(0).getAccesible().size(); i++){
+//            logger.info(OSPFNodes.get(0).getAccesible().get(i).getFirst().toString());
+//        }
+        OSPFNode newNode1 = new OSPFNode(DatapathId.of("01"));
+        OSPFNode newNode2 = new OSPFNode(DatapathId.of("02"));
+        OSPFNode newNode3 = new OSPFNode(DatapathId.of("03"));
+        OSPFNode newNode4 = new OSPFNode(DatapathId.of("04"));
+        newNode1.addToAccesible(IPv4Address.of("10.0.0.1/24"), 0, OFPort.of(1));
+        newNode4.addToAccesible(IPv4Address.of("10.0.1.1/24"), 0, OFPort.of(1));
+        newNode1.addNeighbour(new Triple<>(DatapathId.of("02"), 10, OFPort.of(2)));
+        newNode1.addNeighbour(new Triple<>(DatapathId.of("03"), 15, OFPort.of(3)));
+        newNode2.addNeighbour(new Triple<>(DatapathId.of("01"), 10, OFPort.of(1)));
+        newNode2.addNeighbour(new Triple<>(DatapathId.of("04"), 20, OFPort.of(2)));
+        newNode3.addNeighbour(new Triple<>(DatapathId.of("01"), 15, OFPort.of(1)));
+        newNode3.addNeighbour(new Triple<>(DatapathId.of("04"), 30, OFPort.of(2)));
+        newNode4.addNeighbour(new Triple<>(DatapathId.of("02"), 20, OFPort.of(1)));
+        newNode4.addNeighbour(new Triple<>(DatapathId.of("03"), 30, OFPort.of(2)));
+        OSPFNodes.add(newNode1);
+        OSPFNodes.add(newNode2);
+        OSPFNodes.add(newNode3);
+        OSPFNodes.add(newNode4);
 
+    }
+
+    private void OSPF() {
+        boolean update = false;
+        while (true) {
+            for (OSPFNode ospfNode : OSPFNodes) {
+                for (int i = 0; i < ospfNode.getNeighbour().size(); i++) {
+                    for (OSPFNode ospfNeighbour : OSPFNodes) {
+                        if (ospfNode.getNeighbour().get(i).getFirst().equals(ospfNeighbour.getSWID())) {
+                            for (int j = 0; j < ospfNeighbour.getAccesible().size(); j++) {
+                                update |= ospfNode.addToAccesible(ospfNeighbour.getAccesible().get(j).getFirst(),
+                                        ospfNode.getNeighbour().get(i).getSecond()
+                                                + ospfNeighbour.getAccesible().get(j).getSecond(),
+                                        ospfNode.getNeighbour().get(i).getThird());
+                            }
+                        }
+                    }
+                }
+            }
+            if (!update)
+                break;
+            else
+                update = false;
+        }
+        for (OSPFNode ospfNode : OSPFNodes){
+            IOFSwitch sw = switchService.getSwitch(ospfNode.getSWID());
+            OFFlowAdd.Builder flow = sw.getOFFactory().buildFlowAdd();
+            Match.Builder match = sw.getOFFactory().buildMatch();
+            ArrayList<OFAction> actionList = new ArrayList<OFAction>();
+            OFActionOutput.Builder action = sw.getOFFactory().actions().buildOutput();
+            for (Triple<IPv4Address, Integer, OFPort> accesible : ospfNode.getAccesible()) {
+                match.setExact(MatchField.ETH_TYPE, EthType.IPv4);
+                match.setExact(MatchField.IPV4_DST, accesible.getFirst());
+                action.setMaxLen(0xffFFffFF);
+                action.setPort(accesible.getThird());
+                actionList.add(action.build());
+                flow.setBufferId(OFBufferId.NO_BUFFER);
+                flow.setHardTimeout(0);
+                flow.setIdleTimeout(0);
+                flow.setOutPort(accesible.getThird());
+                flow.setActions(actionList);
+                flow.setMatch(match.build());
+                sfp.addFlow("dhcp-port---" + accesible.getThird().getPortNumber() + "---(" + accesible.getThird().toString() + ")", flow.build(), sw.getId());
+                flow.setPriority(1);
+
+
+            }
+
+
+        }
+
+    }
 }
+
+
+
+
--- src/main/java/net/floodlightcontroller/mactracker/OSPFNode.java
+++ src/main/java/net/floodlightcontroller/mactracker/OSPFNode.java
@@ -2,42 +2,47 @@ package net.floodlightcontroller.mactracker;
 
 import org.projectfloodlight.openflow.types.DatapathId;
 import org.projectfloodlight.openflow.types.IPv4Address;
+import org.projectfloodlight.openflow.types.OFPort;
 
 import java.util.ArrayList;
 
 public class OSPFNode {
     private boolean isSwitch;
-    private ArrayList<Triple<IPv4Address, Integer, Integer>> Accesible;     //ip, cost, port
-    private ArrayList<Triple<DatapathId, Integer, Integer>> neighbour;       //swid, cost, port
+    private DatapathId swid;
+    private ArrayList<Triple<IPv4Address, Integer, OFPort>> Accesible;     //ip, cost, port
+    private ArrayList<Triple<DatapathId, Integer, OFPort>> neighbour;       //swid, cost, port
 
-    public OSPFNode() {
+    public OSPFNode(DatapathId SWID) {
+        this.swid = SWID;
         this.isSwitch = true;
         Accesible = new ArrayList<>();
     }
-    public boolean addToAccesible(IPv4Address newIP, int cost, int port){
-        for (Triple<IPv4Address, Integer, Integer> itr : Accesible){
+    public boolean addToAccesible(IPv4Address newIP, int cost, OFPort port){
+        for (Triple<IPv4Address, Integer, OFPort> itr : Accesible){
             if (newIP.equals(itr.getFirst())){
                 if (cost < itr.getSecond()){
                     itr.setSecond(cost);
                     itr.setThird(port);
                     return true;
                 }
+                else{
+                    return false;
+                }
             }
-            return Accesible.add(new Triple<>(newIP, cost, port));
         }
-        return false;
+        return Accesible.add(new Triple<>(newIP, cost, port));
     }
-    public ArrayList<Triple<IPv4Address, Integer, Integer>> getAccesible(){
+    public ArrayList<Triple<IPv4Address, Integer, OFPort>> getAccesible(){
         return Accesible;
     }
     public boolean isSwitch(){
         return this.isSwitch;
     }
-
-    public void addNeighbour(Triple<DatapathId, Integer, Integer> newNeighbour){
+    public ArrayList<Triple<DatapathId, Integer, OFPort>> getNeighbour(){return neighbour;}
+    public void addNeighbour(Triple<DatapathId, Integer, OFPort> newNeighbour){
         neighbour.add(newNeighbour);
     }
-
+    public DatapathId getSWID(){return swid;}
 
 }
 
